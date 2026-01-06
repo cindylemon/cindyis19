c3812244d7bd202c2d7194014495f5710378815d
cindy <cindy.wu23236@gmail.com>
2026-01-06 13:18:38 -0800

    finished all the text and titles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import React, { Fragment } from 'react';
 
+// extrapolate the text, make it a box that you can scroll for text in
+
 function App() {
   const amberText = "Amber was my first friend in Vancouver, and it actually feels like fate that we ever met." +
     "She really gets me and it’s like we’re in our own world whenever we’re together." +
@@ -28,7 +30,12 @@ function App() {
     "I love this girl so so so much, she’s seen my literal worst days and has been around for some of my best. " +
     "We’ve actually gone to Hell (constant League of Legends grinding) and back together, and I know that she’s always there for me." +
     "She is so funny and genuine and always puts me back in my place, and I think about her all the time."
-  const familyText = ""
+  const familyText = "My parents do so much for me, I am so thankful for their love and support. " +
+    "A plus side of moving back home was that I can finally spend more time with them. " +
+    "They work so hard and I always hope that I make them proud. " +
+    "I am also really glad that my brother and I have started talking more and getting closer. " +
+    "We didn’t really grow up close, but I feel like we’re actually friends now, and he brings me lots of cool presents from when he travels. " +
+    "I also met a lot of extended family over the summer, and they’re all incredibly cool and supportive of me."
   const felText = "I think Felicia was the first friend I made through SUS. " +
     "She’s so funny, and I feel like I can really be myself around her. " +
     "She works so incredibly hard and is so smart, and I love her even though she is chained by the leg to her RA position and can never hang out with me. " +
@@ -49,7 +56,51 @@ function App() {
     "I could have individual sections just for each of them. " +
     "I’m so grateful for all the tournaments and the training, the texts, the FaceTimes, and especially for the terrible (but sometimes seriously good) life and love advice that gets passed around. " +
     "I guess 2 hours straight of physical every Sunday really bonds people together."
-  const macText = ""
+  const macText = "High school badminton was so much fun. " +
+    "From playing pickleball at practice to intense Provincial’s matches, I enjoyed every moment of it. " +
+    "And what made it so special was the team. " +
+    "We had some amazing seasons and I’m so proud of all of us! " +
+    "I miss bussing for hours, walking around random cities, and even the stressing over lineups and brutal losses. " +
+    "I wouldn’t have traded it for the world. " +
+    "So many people on this team have been through so much with me, and were some of my closest friends."
+  const maggieText = "Maggie is one of the funniest and smartest people I know. " +
+    "We were attached at the hip for most of the second sem last year, and I don’t think we stopped laughing the whole time, even through countless man-related crashouts. " +
+    "She really lit up my life, and helped me survive the stress of school and specialization. " +
+    "We don’t see each other often anymore (mostly because she never came to class this year…) but I know we still love each other and I’m so glad that we met."
+  const melissaText = "Melissa is genuinely so special. " +
+    "She’s so funny and diligent, and insanely good at badminton (but arguably better at pickleball). " +
+    "I’m so grateful for all the rides she’s given me, all the time we spent together, and all the times I laughed so hard my stomach hurt. " +
+    "I look back very fondly on my high school summers largely because of her. " +
+    "I miss her jokes and her mannerisms and can’t wait to see her again."
+  const petrosText = "Faith has been something that I have struggled with my whole life (and continue to struggle deeply with). " +
+    "Despite this, I am thankful that I have been led to a group of people who are unreasonably funny and caring. " +
+    "I did not spend an insurmountable amount of time with them, but I truly treasure what we have been through together. " +
+    "I’ve always been so inspired by the casual authenticity, kindness, and joy that these people radiate, and I’m so blessed to have met them."
+  const ponderosaText = "These are a group of my closest friends and I genuinely love them to bits. " +
+    "We met through SUS, and I’m so thankful that we got to meet. " +
+    "They’re the funniest, kindest, most genuine people in the world, and I am so blessed that I can call them my friends. " +
+    "They are all so smart and bright and seriously deserve the whole world. " +
+    "From finals seasons to Forestry fields to freakishly cold water, I can’t imagine anyone better to go through those insanely late nights (or life in general) with."
+  const saraText = "Sara was my best friend growing up. I was always in her house after school and eating her snacks, even when we didn’t have a French puppet show assignment due. " +
+    "She influenced me into becoming emo in middle school, and ultimately introduced me to some insanely good music and artists. " +
+    "She’s one of the funniest and coolest people I know, and I love all the stories she tells me. " +
+    "I’m very blessed to still have her in my life, and grateful that we still visit each other pretty often. "
+  const sophiaText = "These people held me down when we first got to UBC. " +
+    "They’re actually like some of the smartest, funniest, talented, hardworking people I know, and some of my favourite memories were with them. " +
+    "I’ve known Sophia and David for insanely long, and have known Sean since high school. " +
+    "Wreck Beach on the night of the Northern Lights is one of my all-time favourite memories, and I have too many beautiful amazing moments with Sophia to even recount them here… " +
+    "Like drawing on her in middle school or drawing with her over quarantine."
+  const susText = "SUS was one of the most impactful things that I ever did in Vancouver. " +
+    "I genuinely think it changed my life, and am so glad to have been blessed with it. " +
+    "I've met so many amazing people, and have been given so many amazing opportunities and insights." +
+    "Like 85% of my closest friends have been met through SUS, and I wouldn't have it any other way."
+  const yeoText = "I love Yeojin so much, I don’t even think I can describe it with words. " +
+    "She’s so empathetic, understanding, and kind, and is so so so much fun to ragebait. " +
+    "I love her personality, and really think that we’re kindred spirits. " +
+    "She takes care of me so well, and when she’s not hitting/biting/abusing me, she’s teaching me more about love and making me laugh. " +
+    "She’s helped me through terrible, scary things, and has brought me so much joy. " +
+    "Even though she pretends to hate me, I strive to be as cool as her one day."
+
 
   const [selectedPhoto, setSelectedPhoto] = useState(null)
   const photos = [
@@ -65,27 +116,39 @@ function App() {
     { src: "src/assets/kai.png", top: "5.25477707%", left: "6.6%", width: "4.649548924%", title: "kai", text: kaiText },
     { src: "src/assets/lebron.png", top: "52.22505308%", left: "54.53270574%", width: "6.627342124%", title: "the lebron james support gang", text: lebronText },
     { src: "src/assets/mac.png", top: "20.91295117%", left: "34.3%", width: "8.639833449%", title: "my hs badminton team", text: macText },
-    { src: "src/assets/maggie.png", top: "26.22080679%", left: "23.88192333%", width: "9.993060375%", title: "maggie", text: "" },
-    { src: "src/assets/melissa.png", top: "75.93630573%", left: "68.9%", width: "9.993060375%", title: "melissa", text: "" },
-    { src: "src/assets/petros.png", top: "4.40552017%", left: "23.4%", width: "6.349757113%", title: "my petros friends", text: "" },
-    { src: "src/assets/ponderosa.png", top: "41%", left: "65.33%", width: "10.08119362%", title: "the ponderosa princesses", text: "" },
-    { src: "src/assets/sara.png", top: "2.972399151%", left: "56.48%", width: "7.807078418%", title: "sara", text: "" },
-    { src: "src/assets/sophia.png", top: "1.167728238%", left: "74.81%", width: "24.28868841%", title: "sophia, david, and sean", text: "" },
-    { src: "src/assets/sus.png", top: "46.39065817%", left: "86.32%", width: "12.90770298%", title: "ubc sus", text: "" },
-    { src: "src/assets/yeojin.png", top: "71.91%", left: "27.7%", width: "9.576682859%", title: "yeojin", text: "" }
+    { src: "src/assets/maggie.png", top: "26.22080679%", left: "23.88192333%", width: "9.993060375%", title: "maggie", text: maggieText },
+    { src: "src/assets/melissa.png", top: "75.93630573%", left: "68.9%", width: "9.993060375%", title: "melissa", text: melissaText },
+    { src: "src/assets/petros.png", top: "4.40552017%", left: "23.4%", width: "6.349757113%", title: "my petros friends", text: petrosText },
+    { src: "src/assets/ponderosa.png", top: "41%", left: "65.33%", width: "10.08119362%", title: "the ponderosa princesses", text: ponderosaText },
+    { src: "src/assets/sara.png", top: "2.972399151%", left: "56.48%", width: "7.807078418%", title: "sara", text: saraText },
+    { src: "src/assets/sophia.png", top: "1.167728238%", left: "74.81%", width: "24.28868841%", title: "sophia, david, and sean", text: sophiaText },
+    { src: "src/assets/sus.png", top: "46.39065817%", left: "86.32%", width: "12.90770298%", title: "ubc sus", text: susText },
+    { src: "src/assets/yeojin.png", top: "71.91%", left: "27.7%", width: "9.576682859%", title: "yeojin", text: yeoText }
   ]
 
   const getPosition = (photo) => {
     const leftPercent = parseFloat(photo.left);
-    if (leftPercent < 50) {
+    const topPercent = parseFloat(photo.top);
+
+    // hardcode it w 4 if else statements, then try to encapsulate ?
+
+    if (leftPercent < 50) { // && topPercent < 50
       return {
         left: `calc(${photo.left} + ${photo.width} + 1%)`,
-        top: photo.top,
+        top: photo.top, //`calc(${photo.top} + ${photo.height} + 1%)`
         arrowDirection: 'left'
       };
-    } else {
+    }
+    // else if (leftPercent < 50 && topPercent >= 50) {
+    //   return {
+    //     left: `calc(${photo.left} + ${photo.width} + 1%)`,
+    //     top: `calc(${photo.top})`,
+    //     arrowDirection: 'left'
+    //   };
+    // } 
+    else {
       return {
-        right: `calc(100% - ${photo.left})`, // + 1% ?
+        right: `calc(100% - ${photo.left} - 2%)`, // + 1% ?
         top: photo.top,
         arrowDirection: 'right'
       };
@@ -130,14 +193,14 @@ function App() {
                   zIndex: 200
                 }}
               >
-                <h3>{photo.title}</h3>
-                <p className="text">{photo.text.toLowerCase()}</p>
-                <button
+                <h3 className = "textTitle">{photo.title}</h3>
+                <p className="textText">{photo.text.toLowerCase()}</p>
+                {/* <button
                   className="close-btn"
                   onClick={() => setSelectedPhoto(null)}
                 >
                   ×
-                </button>
+                </button> */}
               </div>
             )}
           </div>
