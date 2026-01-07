d0f0177c9e9ab5702bdbd55f9296e2b2f2ab0f52
cindy <cindy.wu23236@gmail.com>
2026-01-06 18:57:58 -0800

    imported photos so that vercel is deployable

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 import amberPng from "./assets/amber.png";
+import bactivePng from "./assets/bactive.png"
+import bcPng from "./assets/bc.png"
+import edmPng from "./assets/edm.png"
+import erinPng from "./assets/erin.png"
+import familyPng from "./assets/family.png"
+import feliciaPng from "./assets/felicia.png"
+import felixPng from "./assets/felix.png"
+import kaiPng from "./assets/kai.png"
+import lebronPng from "./assets/lebron.png"
+import macPng from "./assets/mac.png"
+import maggiePng from "./assets/maggie.png"
+import melissaPng from "./assets/melissa.png"
+import petrosPng from "./assets/petros.png"
+import ponderosaPng from "./assets/ponderosa.png"
+import saraPng from "./assets/sara.png"
+import sophiaPng from "./assets/sophia.png"
+import susPng from "./assets/sus.png"
+import yeojinPng from "./assets/yeojin.png"
 
 // extrapolate the text, make it a box that you can scroll for text in
 
@@ -107,24 +125,24 @@ function App() {
   const photos = [
     // src, top, left, width, title, text
     { src: amberPng, top: "53.3970276%", left: "2.7%", width: "8.743927828%", title: "amber", text: amberText },
-    { src: "src/assets/bactive.png", top: "48.3%", left: "23.21%", width: "9.090909091%", title: "b-active badminton club", text: bactiveText },
-    { src: "src/assets/bc.png", top: "2.441613588%", left: "40.13%", width: "10.99930604%", title: "my bc friends", text: bcText },
-    { src: "src/assets/edm.png", top: "72.23991507%", left: "42.73%", width: "17.34906315%", title: "my edmonton friends", text: edmText },
-    { src: "src/assets/erin.png", top: "67.94055202%", left: "18%", width: "12.17904233%", title: "erin", text: erinText },
-    { src: "src/assets/family.png", top: "74.62845011%", left: "59.02861665%", width: "3.695350451%", title: "my family", text: familyText },
-    { src: "src/assets/felicia.png", top: "55.25477707%", left: "10.2%", width: "16.48160999%", title: "felicia", text: felText },
-    { src: "src/assets/felix.png", top: "24.62845011%", left: "55.25%", width: "8.188063845%", title: "felix", text: felixText },
-    { src: "src/assets/kai.png", top: "5.25477707%", left: "6.6%", width: "4.649548924%", title: "kai", text: kaiText },
-    { src: "src/assets/lebron.png", top: "52.22505308%", left: "54.53270574%", width: "6.627342124%", title: "the lebron james support gang", text: lebronText },
-    { src: "src/assets/mac.png", top: "20.91295117%", left: "34.3%", width: "8.639833449%", title: "my hs badminton team", text: macText },
-    { src: "src/assets/maggie.png", top: "26.22080679%", left: "23.88192333%", width: "9.993060375%", title: "maggie", text: maggieText },
-    { src: "src/assets/melissa.png", top: "75.93630573%", left: "68.9%", width: "9.993060375%", title: "melissa", text: melissaText },
-    { src: "src/assets/petros.png", top: "4.40552017%", left: "23.4%", width: "6.349757113%", title: "my petros friends", text: petrosText },
-    { src: "src/assets/ponderosa.png", top: "41%", left: "65.33%", width: "10.08119362%", title: "the ponderosa princesses", text: ponderosaText },
-    { src: "src/assets/sara.png", top: "2.972399151%", left: "56.48%", width: "7.807078418%", title: "sara", text: saraText },
-    { src: "src/assets/sophia.png", top: "1.167728238%", left: "74.81%", width: "24.28868841%", title: "sophia, david, and sean", text: sophiaText },
-    { src: "src/assets/sus.png", top: "46.39065817%", left: "86.32%", width: "12.90770298%", title: "ubc sus", text: susText },
-    { src: "src/assets/yeojin.png", top: "71.91%", left: "27.7%", width: "9.576682859%", title: "yeojin", text: yeoText }
+    { src: bactivePng, top: "48.3%", left: "23.21%", width: "9.090909091%", title: "b-active badminton club", text: bactiveText },
+    { src: bcPng, top: "2.441613588%", left: "40.13%", width: "10.99930604%", title: "my bc friends", text: bcText },
+    { src: edmPng, top: "72.23991507%", left: "42.73%", width: "17.34906315%", title: "my edmonton friends", text: edmText },
+    { src: erinPng, top: "67.94055202%", left: "18%", width: "12.17904233%", title: "erin", text: erinText },
+    { src: familyPng, top: "74.62845011%", left: "59.02861665%", width: "3.695350451%", title: "my family", text: familyText },
+    { src: feliciaPng, top: "55.25477707%", left: "10.2%", width: "16.48160999%", title: "felicia", text: felText },
+    { src: felixPng, top: "24.62845011%", left: "55.25%", width: "8.188063845%", title: "felix", text: felixText },
+    { src: kaiPng, top: "5.25477707%", left: "6.6%", width: "4.649548924%", title: "kai", text: kaiText },
+    { src: lebronPng, top: "52.22505308%", left: "54.53270574%", width: "6.627342124%", title: "the lebron james support gang", text: lebronText },
+    { src: macPng, top: "20.91295117%", left: "34.3%", width: "8.639833449%", title: "my hs badminton team", text: macText },
+    { src: maggiePng, top: "26.22080679%", left: "23.88192333%", width: "9.993060375%", title: "maggie", text: maggieText },
+    { src: melissaPng, top: "75.93630573%", left: "68.9%", width: "9.993060375%", title: "melissa", text: melissaText },
+    { src: petrosPng, top: "4.40552017%", left: "23.4%", width: "6.349757113%", title: "my petros friends", text: petrosText },
+    { src: ponderosaPng, top: "41%", left: "65.33%", width: "10.08119362%", title: "the ponderosa princesses", text: ponderosaText },
+    { src: saraPng, top: "2.972399151%", left: "56.48%", width: "7.807078418%", title: "sara", text: saraText },
+    { src: sophiaPng, top: "1.167728238%", left: "74.81%", width: "24.28868841%", title: "sophia, david, and sean", text: sophiaText },
+    { src: susPng, top: "46.39065817%", left: "86.32%", width: "12.90770298%", title: "ubc sus", text: susText },
+    { src: yeojinPng, top: "71.91%", left: "27.7%", width: "9.576682859%", title: "yeojin", text: yeoText }
   ]
 
   const getPosition = (photo) => {
